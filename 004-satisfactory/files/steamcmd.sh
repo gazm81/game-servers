416910859b202f49ee8ad33a0aa4425f221aff0e
Garry Hughes <you@example.com>
2023-01-29 15:53:09 +1100

    working version of satisfactory

--- 004-satisfactory/files/steamcmd.sh
+++
@@ -1,9 +0,0 @@
-apt update && sudo apt upgrade
-apt install -y software-properties-common
-add-apt-repository multiverse
-dpkg --add-architecture i386
-apt update
-apt install -y lib32gcc-s1 steamcmd
-
-##
-
--- 004-satisfactory/files/webmin.sh
+++
@@ -1,6 +0,0 @@
-apt update
-echo "deb http://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list
-wget -q -O- http://www.webmin.com/jcameron-key.asc | sudo apt-key add
-apt update
-apt install -y webmin
-ufw allow 10000
